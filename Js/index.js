21b869d1d6b935f1d470fe2596027c0b1d019ba8
Samira-Hoummane <samirahoummane@gmail.com>
2021-10-05 12:33:34 +0100

    background edited

---
+++ Js/index.js
@@ -0,0 +1,16 @@
+var app = document.querySelector('.txt-animation');
+
+var typewriter = new Typewriter(app, {
+  loop: true,
+  delay: 75,
+});
+
+typewriter
+  .pauseFor(2500)
+  .typeString('I am Samira Hoummane, a web developer')
+  .pauseFor(300)
+  .deleteChars(13)
+  .typeString('<strong> front-end</strong> developer ')
+  .typeString('<strong> and an <span style="color: #57221F;"> artist </span> ! </strong>')
+  .pauseFor(1000)
+  .start();
